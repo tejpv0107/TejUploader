14e9d8b13704608adcbb316879bf697923f1fdf8
tejpv0107 <tejpv0107@gmail.com>
2026-07-18 19:18:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ import os
 class Config(object):
     BOT_TOKEN = "8892087351:AAGO42IzvzZs0-n6zfMrAOjqT-eltriUYqc"
     API_ID = 28331645
-    API_HASH = 021a31c647f7a20ad490b7ab5ee2f150
+    API_HASH = "021a31c647f7a20ad490b7ab5ee2f150"
     VIP_USER = os.environ.get('VIP_USERS', '6501759311').split(',')
     VIP_USERS = [int(user_id) for user_id in VIP_USER]
